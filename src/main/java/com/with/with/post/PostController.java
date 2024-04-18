125a416c9c05999edd185e9de8fc9e164b0d5457
이창민 <2ckdaks@gmail.com>
2024-04-18 22:10:44 +0900

    삭제기능 구현완료

--- src/main/java/com/with/with/post/PostController.java
+++ src/main/java/com/with/with/post/PostController.java
@@ -1,13 +1,12 @@
 package com.with.with.post;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +16,7 @@ import java.util.Optional;
 public class PostController {
 
     private final PostService postService;
+    private final PostRepository postRepository;
 
     @GetMapping("/write")
     public String write(){
@@ -65,5 +65,16 @@ public class PostController {
     public String updatePost(@PathVariable Long id, @ModelAttribute PostDto postDto, Authentication authentication) {
         postService.updatePost(postDto, id, authentication);
         return "redirect:/detail/" + id;
+    }
+
+    //query string으로 받은경우
+    @DeleteMapping("/post")
+    public ResponseEntity<?> deletePost(@RequestParam Long id){
+        try {
+            postRepository.deleteById(id);
+            return ResponseEntity.ok().build(); // 성공적으로 처리되었다는 응답
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("삭제 실패");
+        }
     }
 }
