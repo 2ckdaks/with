6152217767ac3f50c399323796ebb8038b83a200
이창민 <2ckdaks@gmail.com>
2024-04-30 07:17:51 +0900

    채팅기능 구현완료

--- src/main/java/com/with/with/chat/ChatController.java
+++ src/main/java/com/with/with/chat/ChatController.java
@@ -1,5 +1,6 @@
 package com.with.with.chat;
 
+import com.with.with.member.CustomUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -17,20 +18,21 @@ public class ChatController {
 
     private final SimpMessagingTemplate template;
 
-    // 생성자 주입만 사용
+    // 생성자 주입을 사용합니다.
     public ChatController(SimpMessagingTemplate template) {
         this.template = template;
     }
 
     @GetMapping("/chat/{postId}")
     public String getChatPage(@PathVariable String postId, Model model) {
-        model.addAttribute("roomId", postId);
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("roomId", postId);
         if (auth != null && auth.isAuthenticated()) {
+            CustomUser user = (CustomUser) auth.getPrincipal();
             ChatMessage joinMessage = new ChatMessage();
             joinMessage.setType(ChatMessage.MessageType.JOIN);
-            joinMessage.setSender(auth.getName());
-            joinMessage.setContent(auth.getName() + " 님이 입장하셨습니다.");
+            joinMessage.setSender(user.getUsername());
+            joinMessage.setContent(user.getUsername() + " 님이 입장하셨습니다.");
 
             // 메시지를 해당 채팅방의 모든 구독자에게 전송
             template.convertAndSend("/topic/" + postId, joinMessage);
@@ -38,25 +40,23 @@ public class ChatController {
         return "chat.html";
     }
 
-
     @MessageMapping("/chat.sendMessage/{roomId}")
     @SendTo("/topic/{roomId}")
-    public ChatMessage sendMessage(@DestinationVariable String roomId, ChatMessage chatMessage) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        chatMessage.setSender(auth.getName()); // 현재 사용자의 이름을 설정
-        System.out.println("Sending message: " + chatMessage.getContent() + " from " + auth.getName());
+    public ChatMessage sendMessage(@DestinationVariable String roomId, ChatMessage chatMessage, Authentication authentication) {
+        CustomUser user = (CustomUser) authentication.getPrincipal();
+        chatMessage.setSender(user.getUsername()); // 현재 사용자의 이름을 설정
+        System.out.println("Sending message: " + chatMessage.getContent() + " from " + user.getUsername());
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser/{roomId}")
     @SendTo("/topic/{roomId}")
-    public ChatMessage addUser(@DestinationVariable String roomId, ChatMessage chatMessage) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        chatMessage.setContent(auth.getName() + " has joined the chat!");
+    public ChatMessage addUser(@DestinationVariable String roomId, ChatMessage chatMessage, Authentication authentication) {
+        CustomUser user = (CustomUser) authentication.getPrincipal();
+        chatMessage.setContent(user.getUsername() + " has joined the chat!");
         chatMessage.setType(ChatMessage.MessageType.JOIN);
-        chatMessage.setSender(auth.getName());
-        System.out.println("User added: " + auth.getName());
+        chatMessage.setSender(user.getUsername());
+        System.out.println("User added: " + user.getUsername());
         return chatMessage;
     }
-
 }
